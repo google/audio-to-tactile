329cfd84e0f979dd56997c82589fd605bb1eb6b7
Pascal Getreuer <getreuer@google.com>
2019-05-29 11:31:55 -0700

    Suppress unused variable warning on kMaxChannels

--- audio/tactile/util.c
+++ audio/tactile/util.c
@@ -1,11 +1,11 @@
 /* Copyright 2019 Google LLC
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     https://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -100,6 +100,7 @@ void PermuteWaveformChannels(const int* permutation, float* waveform,
                              int num_frames, int num_channels) {
   float input_frame[32];
   const int kMaxChannels = sizeof(input_frame) / sizeof(*input_frame);
+  (void)kMaxChannels;
   assert(1 <= num_channels && num_channels <= kMaxChannels);
   int i;
   for (i = 0; i < num_frames; ++i, waveform += num_channels) {
